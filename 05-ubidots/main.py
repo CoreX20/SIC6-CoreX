7dcb26e7b671b7176f382bef942d0a07d51c789f
CoreX20 <korneliussetiawan13@gmail.com>
2025-02-25 15:59:18 +0700

    Add PIR sensor and send sensor data to Flask server

--- 05-ubidots/main.py
+++ 05-ubidots/main.py
@@ -1,6 +1,7 @@
 from machine import Pin
-from umqtt.simple import MQTTClient
-import ujson
+
+# from umqtt.simple import MQTTClient
+# import ujson
 import network
 import utime as time
 import dht
@@ -9,41 +10,83 @@ import urequests as requests
 DEVICE_ID = "sic6corex"
 WIFI_SSID = "Casablanca 2.4"
 WIFI_PASSWORD = "61174desamenganti"
-MQTT_BROKER = "sic6sparks.shiftr.io"
-MQTT_CLIENT = DEVICE_ID
-MQTT_TELEMETRY_TOPIC = "iot/telemetry"
-MQTT_CONTROL_TOPIC = "iot/control"
+# MQTT_BROKER = "sic6sparks.shiftr.io"
+# MQTT_CLIENT = DEVICE_ID
+# MQTT_TELEMETRY_TOPIC = "iot/telemetry"
+# MQTT_CONTROL_TOPIC = "iot/control"
 TOKEN = "BBUS-yzTeYERqDXIbREp17mfFoVvraflJiv"
 DHT_PIN = Pin(5)
+PIR_PIN = Pin()
+dht_sensor = dht.DHT11(DHT_PIN)
+pir_sensor = None
 
-def did_receive_callback(topic, message):
-    print('\n\nData Received! \ntopic = {0}, message = {1}'.format(topic, message))
+server_url = "http://192.168.0.1/sensor_data"  # Ubah sesuai Flask server IP address
 
-def create_json_data(temperature, humidity):
-    data = ujson.dumps({
-        "device_id": DEVICE_ID,
-        "temp": temperature,
-        "humidity": humidity,
-        "type": "sensor"
-    })
-    return data
+# def did_receive_callback(topic, message):
+#     print('\n\nData Received! \ntopic = {0}, message = {1}'.format(topic, message))
 
-def mqtt_client_publish(topic, data):
-    print("\nUpdating MQTT Broker...")
-    mqtt_client.publish(topic, data)
-    print(data)
+# def create_json_data(temperature, humidity):
+#     data = ujson.dumps({
+#         "device_id": DEVICE_ID,
+#         "temp": temperature,
+#         "humidity": humidity,
+#         "type": "sensor"
+#     })
+#     return data
 
-def send_data(temperature, humidity):
+
+# def mqtt_client_publish(topic, data):
+#     print("\nUpdating MQTT Broker...")
+#     mqtt_client.publish(topic, data)
+#     print(data)
+
+
+def read_dht_data():
+    try:
+        dht_sensor.measure()
+        humidity = dht_sensor.humidity()
+        temperature = dht_sensor.temperature()
+        return humidity, temperature
+    except Exception as e:
+        print(f"Error reading DHT sensor: {e}")
+        return None, None
+
+
+def read_pir_data():
+    try:
+        return pir_sensor.value()
+    except Exception as e:
+        print(f"Error reading PIR sensor: {e}")
+        return None
+
+
+def send_data_to_ubidots(temperature, humidity, motion):
     url = "http://industrial.api.ubidots.com/api/v1.6/devices/" + DEVICE_ID
     headers = {"Content-Type": "application/json", "X-Auth-Token": TOKEN}
-    data = {
-        "temp": temperature,
-        "humidity": humidity,
-    }
+    data = {"temp": temperature, "humidity": humidity, "motion": motion}
     response = requests.post(url, json=data, headers=headers)
-    print("Done Sending Data!")
-    print("Response:", response.text)
 
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        if response.status_code == 200:
+            print("Data sent successfully to Ubidots!")
+        else:
+            print(f"Error sending data to Ubidots. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error sending data to Ubidots: {e}")
+
+
+def send_data_to_flask(sensor_type, sensor_id, sensor_data):
+    data = {"sensor_id": sensor_id, "sensor_type": sensor_type, "value": sensor_data}
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(server_url, json=data, headers=headers)
+        print("Response from Flask:", response.json())
+    except Exception as e:
+        print("Failed to send data to Flask:", e)
+
+
+# Wi-Fi Connection
 wifi_client = network.WLAN(network.STA_IF)
 wifi_client.active(True)
 print("Connecting device to WiFi", end="")
@@ -55,23 +98,22 @@ while not wifi_client.isconnected():
 print("WiFi Connected!")
 print(wifi_client.ifconfig())
 
-dht_sensor = dht.DHT11(DHT_PIN)
-telemetry_data_old = ""
-
 while True:
-    try:
-        dht_sensor.measure()
-    except:
-        pass
+    humidity, temperature = read_dht_data()
+    if humidity is not None and temperature is not None:
+        print(f"Humidity: {humidity}% Temperature: {temperature}°C")
+        dht_data = {"humidity": humidity, "temperature": temperature}
+        send_data_to_flask("DHT", "dht_01", dht_data)
+    else:
+        print("Error reading DHT data, skipping Flask and Ubidots updates.")
 
-    time.sleep(0.25)
+    motion_detected = read_pir_data()
+    if motion_detected is not None:
+        print(f"Motion Detected: {motion_detected}")
+        pir_data = {"motion": motion_detected}
+        send_data_to_flask("PIR", "pir_01", pir_data)
 
-    telemetry_data_new = create_json_data(dht_sensor.temperature(), dht_sensor.humidity())
+    if humidity is not None and temperature is not None and motion_detected is not None:
+        send_data_to_ubidots(temperature, humidity, motion_detected)
 
-    if telemetry_data_new != telemetry_data_old:
-        telemetry_data_old = telemetry_data_new
-        
-        # Call the send_data function to send data to Ubidots
-        send_data(dht_sensor.temperature(), dht_sensor.humidity())
-    
-    time.sleep(0.25)
+    time.sleep(2)
